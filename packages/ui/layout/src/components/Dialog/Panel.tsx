af542652713de228d008ac5c418dc907340f7fed
stupside <kilian.houpeurt@gmail.com>
2024-02-18 22:50:25 +0100

    add layout rollup package

---
+++ packages/ui/layout/src/components/Dialog/Panel.tsx
@@ -0,0 +1,16 @@
+import { Dialog } from "@headlessui/react";
+
+import { forwardRef, type PropsWithChildren } from "react";
+
+const DialogPanel = forwardRef<any, PropsWithChildren>(({ children }, ref) => {
+  return (
+    <Dialog.Panel
+      ref={ref}
+      className="max-w-md transform overflow-hidden transition-all bg-zinc-100 p-5 rounded-md text-zinc-800"
+    >
+      {children}
+    </Dialog.Panel>
+  );
+});
+
+export default DialogPanel;
