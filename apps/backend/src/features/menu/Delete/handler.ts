287cac42866d54fb06899d52b7ffe663afe2746a
noneofyourbusinessbaby <144385516+noneofyourbusinessbaby@users.noreply.github.com>
2024-03-07 16:47:23 +0000

    feature(backend): handler added for deleting

--- apps/backend/src/features/menu/Delete/handler.ts
+++ apps/backend/src/features/menu/Delete/handler.ts
@@ -1,9 +1,22 @@
 import { MyRoute } from '../../../fastify'
-
+import prisma from '../../../utils/prisma'
 import { Interface } from './schema'
 
 // c'est ici
 export const Handler: MyRoute<Interface> = () => async (request, response) => {
-  void request
-  return await response.send()
+  const identity = request.requestContext.get('identity')
+
+  if (identity === undefined) throw new Error('Unauthorized')
+
+  const menuToDelete = await prisma.menu.findUnique({
+    where: {
+      id: request.params.menuId, // Unique identifier for the menu
+    },
+    include: {
+      event: true,
+    },
+  })
+
+  if (menuToDelete === null) return response.notFound()
+  if (menuToDelete.event.userId != identity.user) return response.unauthorized()
 }
