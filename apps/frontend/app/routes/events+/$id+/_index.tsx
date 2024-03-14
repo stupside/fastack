6d60ac1f7efabd553fc994f76d14098d8ab5155e
luaroncrew <ma-kir.ru@yandex.ru>
2024-03-14 16:32:48 +0100

    fix selected layout

--- apps/frontend/app/routes/events+/$id+/_index.tsx
+++ apps/frontend/app/routes/events+/$id+/_index.tsx
@@ -224,87 +224,118 @@ const PageComponent: FC = () => {
 
   return (
     <>
-      <header className="mb-4 w-full flex flex-row justify-center items-center">
+      <header className="m-10 w-full flex flex-row justify-center items-center">
         <h1 className="text-2xl mx-4">{title}</h1>
         <h2>{date}</h2>
       </header>
       <div className="px-3 w-full flex flex-row justify-center">
-        <div className="grid gap-2 sm:grid-cols-1 lg:grid-cols-2 max-w-2xl m-1">
-          {menus.map((menu) => {
-            const selected = menu.id === selection
-            return (
-              <div>
-                <div
-                  className={
-                    selected
-                      ? 'border-4 border-pink-400'
-                      : 'border-4 border-zinc-50'
-                  }
-                  onClick={() => setSelection(menu.id)}
-                >
-                  <article key={menu.id} className={'flex flex-col w-80'}>
-                    <Tooltip content={menu.description}>
-                      <div className="bg-sky-500 h-10 w-80 flex flex-row justify-center items-center">
+        <div>
+          <div className="grid gap-2 sm:grid-cols-1 lg:grid-cols-2 max-w-2xl m-1">
+            {menus.map((menu) => {
+              const selected = menu.id === selection
+              return (
+                  <div>
+                    <div
+                        className={
+                          selected
+                              ? 'border-4 border-pink-400'
+                              : 'border-4 border-zinc-50'
+                        }
+                        onClick={() => setSelection(menu.id)}
+                    >
+                      <article key={menu.id} className={'flex flex-col w-80 hover:opacity-75 hover:cursor-pointer'}>
+                        <Tooltip content={menu.description}>
+                          <div className="bg-sky-500 h-10 w-80 flex flex-row justify-center items-center">
                         <span className="text-white font-thin mx-4">
                           {menu.name}
                         </span>
-                        <MenuTooltipIcon />
-                      </div>
-                    </Tooltip>
-                    {menu.dishes.map((dish) => (
-                      <div className="border border-black h-10 w-80 border-t-0 flex flex-row justify-center items-center m-0 p-0">
+                            <MenuTooltipIcon/>
+                          </div>
+                        </Tooltip>
+                        {menu.dishes.map((dish) => (
+                            <div
+                                className="border border-black h-10 w-80 border-t-0 flex flex-row justify-center items-center m-0 p-0">
                         <span className="text-black font-thin mx-4">
                           {dish.name}
                         </span>
-                      </div>
-                    ))}
-                  </article>
-                </div>
-                {menu.allergens.map((allergen) => {
-                  return (
-                    <DietConstraint key={allergen} dietConstraint={allergen}>
-                      <FontAwesomeIcon icon={faWheatAlt} />
-                      <span className={'opacity-100 font-thin mx-4 text-sm'}>
+                            </div>
+                        ))}
+                      </article>
+                    </div>
+                    {menu.allergens.map((allergen) => {
+                      return (
+                          <DietConstraint key={allergen} dietConstraint={allergen}>
+                            <FontAwesomeIcon icon={faWheatAlt}/>
+                            <span className={'opacity-100 font-thin mx-4 text-sm'}>
                         {allergen}
                       </span>
-                    </DietConstraint>
-                  )
-                })}
-              </div>
-            )
-          })}
+                          </DietConstraint>
+                      )
+                    })}
+                  </div>
+              )
+            })}
+          </div>
         </div>
-        <div className="w-80 flex flex-col justify-start items-center m-2">
-          <div className="text-xl align-middle">Your selection</div>
+        <div className="w-80 flex flex-col justify-start items-center mx-10">
           {menus.map((menu) => {
             if (menu.id !== selection) {
               return
             }
             return (
-              <div>
-                <div className="text-2xl my-2">{menu.name}</div>
-                <div>{menu.description}</div>
-              </div>
+                <div>
+                  <div className="text-2xl my-2">{menu.name}</div>
+                  <div>{menu.description}</div>
+                  {menu.dishes.map((dish) => (
+                      <>
+                        <div className="flex flex-col justify-center w-80 my-3">
+                          <div className="flex flex-row justify-start items-center">
+                          <span className="text-black font-thin text-lg">
+                            {dish.name}
+                          </span>
+                          </div>
+                          <div className="flex flex-row justify-start items-center">
+                          <span className="text-black font-thin text-xs">
+                            {dish.description}
+                          </span>
+                          </div>
+                        </div>
+
+                        {dish.ingredients.map((ingredient) => {
+                          return <>
+                            <div>
+                              <FontAwesomeIcon icon={faWheatAlt}/>
+                              <span className={'opacity-100 font-thin mx-4 text-sm'}>
+                               {ingredient.name}
+                             </span>
+                            </div>
+                          </>
+                        })}
+                      </>
+                  ))}
+                  <div className="w-full flex flex-row justify-start my-10">
+                    <button
+                        className="p-1 bg-[#0e1729] text-white font-thin w-52 text-sm hover:opacity-75">
+                      Submit</button>
+                  </div>
+                </div>
             )
           })}
         </div>
-      </div>
-      <div className="w-full flex flex-row justify-center m-4">
-        <button className="p-2 bg-[#0e1729] text-white w-72"> Submit</button>
       </div>
     </>
   )
 }
 
 const DietConstraint: FC<
-  PropsWithChildren<{
-    dietConstraint: string
-  }>
-> = ({ children, dietConstraint }) => {
+    PropsWithChildren<{
+      dietConstraint: string
+    }>
+> = ({children, dietConstraint}) => {
   if (dietConstraint !== 'Gluten') {
     return (
-      <div
-        className={
+        <div
+            className={
           'text-black h-10 w-80 border-t-0 opacity-100 flex flex-row justify-start items-center m-0 p-0'
         }
       >
